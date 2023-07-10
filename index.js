b0cbf901f3b46b5f4b46c95f4326a13790747646
not-damien <93546644+not-damien@users.noreply.github.com>
2023-07-10 13:48:52 -0700

    added method stubs

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/login',(req,res)=>{
+    //todo
+    //sanitize 
+
+    //validate 
+
+    //on succsess generate new JWT and send in back in response 
+    //on failure seend approprate error message and dont update database
+})
+
+
+app.post('/register',(req,res)=>{
+  //todo
+  //sanitize 
+
+  //validate 
+
+  //on succsess generate new user in database JWT and send in back in response 
+  //on failure seend approprate error message and dont update database
+})
+
+
+app.get('/logout', (req, res) => {
+  res.send('Logged Out')
+})
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
